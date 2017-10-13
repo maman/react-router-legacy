136fd940790ab891befe84fa4d0359aaab5d9d81
Achmad Mahardi <achmad@mahardi.me>
2017-10-13 12:09:08 +0700

    Adjust examples

--- examples/nested-animations/app.js
+++ examples/nested-animations/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import ReactCSSTransitionGroup from 'react-transition-group'
 import { render } from 'react-dom'
 import { browserHistory, Router, Route, Link } from 'react-router'
 
